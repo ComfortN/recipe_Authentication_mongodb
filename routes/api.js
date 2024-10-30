d20655f8c92bc6c01f4ededddb63b560b9d51aa8
Comfort Ngwenya <>
2024-10-30 11:26:17 +0200

    Adding the authentication and authorization on the routes

--- routes/api.js
+++ routes/api.js
@@ -1,14 +1,20 @@
 import express from "express"
 import recipeController from "../controllers/recipeController.js"
 import { validateCreateRecipe, validatePagination, validateRecipeId, validateUpdateRecipe } from "../middleware/recipeValidation.js"
+import { validateRegister, validateLogin } from "../middleware/userValidation.js"
+import { protect, authorize } from "../middleware/auth.js"
+import { register, login } from "../controllers/userController.js"
 
 const router = express.Router()
 
-router.post('/recipes', validateCreateRecipe, recipeController.CreateRecipe)
+router.post('/auth/register', validateRegister, register);
+router.post('/auth/login', validateLogin, login);
+
+router.post('/recipes', protect, authorize('admin'), validateCreateRecipe, recipeController.CreateRecipe)
 router.get('/recipes', validatePagination, recipeController.getRecipes)
 router.get('/recipes/:id', validateRecipeId, recipeController.getRecipe)
-router.put('/recipes/:id', [...validateRecipeId, validateUpdateRecipe], recipeController.updateRecipe)
-router.delete('/recipes/:id', validateRecipeId, recipeController.deleteRecipe)
+router.put('/recipes/:id', protect, authorize('admin'), [...validateRecipeId, validateUpdateRecipe], recipeController.updateRecipe)
+router.delete('/recipes/:id', protect, authorize('admin'), validateRecipeId, recipeController.deleteRecipe)
 
 
 export default router;
